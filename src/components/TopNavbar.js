e369160064b61f7f875f8ababa5ef6d21cf7d431
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-05-21 17:31:39 +0900

    TopNavbar

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { AppBar, Box, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Box, Container, Drawer, IconButton, List, ListItem, ListItemButton, Stack, styled, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { PrimaryButton, TextButton } from './styledComponents';
 
+const CustomizedDrawer = styled(Drawer)`
+  .MuiPaper-root {
+    background-color: #111;
+  }
+`;
+
 export default function TopNavbar() {
+  const [drawerOpened, setDrawerOpened] = useState(false);
+
   return (
     <AppBar position="static" sx={{ backgroundColor: 'rgba(0, 0, 0, 0)', py: 3 }}>
       <Container maxWidth="xl">
@@ -21,9 +29,29 @@ export default function TopNavbar() {
           <TextButton sx={{ mr: 4, fontWeight: 600, display: { xs: 'none', md: 'flex' } }}>Explore Dice</TextButton>
           <PrimaryButton variant="contained">Connect</PrimaryButton>
 
-          <IconButton>
+          <IconButton
+            size="large"
+            sx={{ color: '#FFFFFF', ml: { xs: 2, md: 0 }, display: { xs: 'flex', md: 'none' } }}
+            onClick={() => setDrawerOpened(true)}
+          >
             <MenuIcon />
           </IconButton>
+
+          <CustomizedDrawer
+            anchor="right"
+            open={drawerOpened}
+            onClose={() => setDrawerOpened(false)}
+          >
+            <Box my={3}>
+              <Stack direction="row" justifyContent="center" alignItems="center">
+                <Box component="img" src="/assets/images/logo.png" />
+              </Stack>
+              <List sx={{ mt: 2 }}>
+                <ListItem><ListItemButton sx={{ color: '#9DB7BD', fontFamily: "'Source Sans Pro', sans-serif" }}>FAQ</ListItemButton></ListItem>
+                <ListItem><ListItemButton sx={{ color: '#9DB7BD', fontFamily: "'Source Sans Pro', sans-serif" }}>Explore Dice</ListItemButton></ListItem>
+              </List>
+            </Box>
+          </CustomizedDrawer>
         </Toolbar>
       </Container>
     </AppBar>
