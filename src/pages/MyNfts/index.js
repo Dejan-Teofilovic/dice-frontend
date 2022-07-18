bb5ec0f354c41b0606561a0116947bb7057233d6
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-07-18 18:31:43 +0900

    The gap between the nfts wider

--- src/pages/MyNfts/index.js
+++ src/pages/MyNfts/index.js
@@ -26,7 +26,7 @@ export default function MyNfts() {
           My NFTs
         </Typography>
       </MotionDiv>
-      <Grid container spacing={2}>
+      <Grid container spacing={4}>
         {
           nfts.map((nftItem, index) => (
             <Grid item xs={12} sm={3} md={2} key={index}>
